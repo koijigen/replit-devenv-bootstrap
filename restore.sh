5af478e8af6262ca00d0b21bb817aa50a5dcfd10
koijigen <104559703+koijigen@users.noreply.github.com>
2022-08-09 08:12:11 +0000

    Create proto type.

---
+++ restore.sh
@@ -0,0 +1,17 @@
+export PJT_HOME=`pwd`
+
+[ ! -d $PJT_HOME/dotfiles ] && git clone https://github.com/koijigen/dotfiles.git
+
+
+cp $PJT_HOME/dotfiles/.bashrc ~/.bashrc
+cp $PJT_HOME/dotfiles/.vim/.vimrc ~/.vimrc
+cp $PJT_HOME/dotfiles/.bash_history ~/.bash_history
+cp $PJT_HOME/dotfiles/.tmux.conf ~/.tmux.conf
+
+sed -e '1s/^/let \$VIM = \"$PJT_HOME\/dotfiles\/.vim\"\n/g' $PJT_HOME/dotfiles/.vim/.vimrc > ~/.vimrc
+echo "" >> ~/.vimrc
+echo "let \$VIMRUNTIME = \"$PJT_HOME/dotfiles/.vim/runtime\"" >> ~/.vimrc
+echo "let \$PJT_HOME = \"$PJT_HOME\"" >> ~/.vimrc
+
+
+echo "restored."
