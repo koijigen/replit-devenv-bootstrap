325817d0d7026a5d4abd51f6ea2e2e99c6ba94e5
koijigen <104559703+koijigen@users.noreply.github.com>
2022-08-13 06:18:12 +0000

    Stop updating `~/.vimrc` in `restore.sh`.

--- restore.sh
+++ restore.sh
@@ -7,10 +7,5 @@ cp $PJT_HOME/dotfiles/.bashrc ~/.bashrc
 cp $PJT_HOME/dotfiles/.vim/.vimrc ~/.vimrc
 cp $PJT_HOME/dotfiles/.tmux.conf ~/.tmux.conf
 
-sed -e '1s/^/let \$VIM = \"$PJT_HOME\/dotfiles\/.vim\"\n/g' $PJT_HOME/dotfiles/.vim/.vimrc > ~/.vimrc
-echo "" >> ~/.vimrc
-echo "let \$VIMRUNTIME = \"$PJT_HOME/dotfiles/.vim/runtime\"" >> ~/.vimrc
-echo "let \$PJT_HOME = \"$PJT_HOME\"" >> ~/.vimrc
-
 
 echo "restored."
