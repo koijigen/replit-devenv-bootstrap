aafa340d7f1609f40642dfb1f608bfe703beaaee
koijigen <104559703+koijigen@users.noreply.github.com>
2022-09-24 05:38:39 +0000

    Suppport fallbak.

--- restore.sh
+++ restore.sh
@@ -3,6 +3,10 @@ export PJT_HOME=`pwd`
 [ ! -d $PJT_HOME/dotfiles ] && git clone https://github.com/koijigen/dotfiles.git
 
 
+cp ~/.bashrc ~/.bashrc.restorebak
+cp ~/.vimrc ~/.vimrc.restorebak
+cp ~/.tmux.conf ~/.tmux.conf.restorebak
+
 cp $PJT_HOME/dotfiles/.bashrc ~/.bashrc
 cp $PJT_HOME/dotfiles/.vim/.vimrc ~/.vimrc
 cp $PJT_HOME/dotfiles/.tmux.conf ~/.tmux.conf
